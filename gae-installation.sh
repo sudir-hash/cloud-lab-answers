9a0d8054b11c7710af1d7e2137451787634df727
Sudhir <sudirsha02@gmail.com>
2023-09-21 05:59:58 +0530

    Create gae-installation.sh

---
+++ gae-installation.sh
@@ -0,0 +1,6 @@
+# GAE installation
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-447.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-447.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
