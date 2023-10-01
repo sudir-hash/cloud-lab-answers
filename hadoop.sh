90d16d6ec4495e7ae36355143c0314f9c8f6c0a0
Sudhir <sudirsha02@gmail.com>
2023-10-01 18:23:32 +0530

    Update hadoop.sh

--- hadoop.sh
+++ hadoop.sh
@@ -1,11 +1,10 @@
 sudo true # just to prompt password once
-
-
-#install jdk
+# install jdk
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt install openjdk-11-jdk -y
-#set path and source
+
+# set path and source
 echo "export JAVA_HOME=\"/usr/lib/jvm/java-11-openjdk-amd64\"" >> $HOME/.profile
 echo "export HADOOP_HOME=\"/opt/hadoop-3.3.1\"" >> $HOME/.profile
 source $HOME/.profile
@@ -15,22 +14,23 @@ echo $JAVA_HOME
 echo $HADOOP_HOME
 echo 'java installation complete'
 
-#download all required files
+# download all required files
 wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1.tar.gz
 wget https://raw.githubusercontent.com/ErikSchierboom/sentencegenerator/master/samples/the-king-james-bible.txt
 
-#extract hadoop and install 
+# extract hadoop and install 
 tar xvf hadoop-3.3.1.tar.gz
 sudo cp -r hadoop-3.3.1 "$HADOOP_HOME"
-#update path and source
+
+# update path and source
 echo "export PATH=\"\$PATH:\$HADOOP_HOME/bin\"" >> $HOME/.profile
 source $HOME/.profile
 
 
-#cleanup
-rm -rf hadoop-3.3.1 hadoop-3.3.1.tar.gz
+# cleanup
+# rm -rf hadoop-3.3.1 hadoop-3.3.1.tar.gz
 
-#run hadoop
+# run hadoop
 mkdir input
 mv the-king-james-bible.txt input
-hadoop jar "$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar" wordcount input output
+/opt/hadoop-3.3.1/bin/hadoop jar "$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar" wordcount input output
