f1311d567dbcac543680c149b204a74727e72ca9
Sudhir <sudirsha02@gmail.com>
2023-10-02 09:47:55 +0530

    Update gae-web.sh

--- gae-web.sh
+++ gae-web.sh
@@ -1,7 +1,7 @@
 echo "Checking for gcloud"
 echo $(gcloud --version)
 # clone repo
-git clone -b gae-web git@github.com:sudir-hash/cloud-lab-answers.git
+git clone -b gae-web https://github.com/sudir-hash/cloud-lab-answers
 
 # run using dev_appserver
 dev_appserver.py cloud-lab-answers/app.yaml
