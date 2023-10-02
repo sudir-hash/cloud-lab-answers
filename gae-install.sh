2312505814965d4bc799299f3ff1cbba47182d3b
Sudhir <sudirsha02@gmail.com>
2023-10-02 09:48:14 +0530

    Update gae-install.sh

--- gae-install.sh
+++ gae-install.sh
@@ -6,4 +6,5 @@ sudo apt install git -y
 sudo wget -q -O - "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-447.0.0-linux-x86_64.tar.gz" --no-check-certificate | tar xvz 
 ./google-cloud-sdk/install.sh
 ./google-cloud-sdk/bin/gcloud init
+source ~/.bashrc
 
