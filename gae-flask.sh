30a6b813ceccce4d94ab409f9a72199b9dc92ab2
Sudhir <sudirsha02@gmail.com>
2023-10-04 18:30:52 +0530

    Update gae-flask.sh

--- gae-flask.sh
+++ gae-flask.sh
@@ -5,4 +5,4 @@ echo $(./google-cloud-sdk/bin/gcloud --version)
 git clone -b gae-flask https://github.com/sudir-hash/cloud-lab-answers
 
 # run using dev_appserver
-dev_appserver.py cloud-lab-answers/app.yaml
+python3 ./google-cloud-sdk/bin/dev_appserver.py ./cloud-lab-answers/app.yaml
