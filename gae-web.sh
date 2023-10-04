4738861dbea38b1a3a34a6fa10269d265e93a366
Sudhir <sudirsha02@gmail.com>
2023-10-04 18:31:06 +0530

    Update gae-web.sh

--- gae-web.sh
+++ gae-web.sh
@@ -4,4 +4,4 @@ echo $(gcloud --version)
 git clone -b gae-web https://github.com/sudir-hash/cloud-lab-answers
 
 # run using dev_appserver
-dev_appserver.py cloud-lab-answers/app.yaml
+python3 ./google-cloud-sdk/bin/dev_appserver.py ./cloud-lab-answers/app.yaml
